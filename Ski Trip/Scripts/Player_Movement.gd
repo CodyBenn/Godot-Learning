c02e46e1d904e4bcf5e52eb9d97cbeb7108f5dd3
CodyBenn <zolnyy@yahoo.com>
2023-09-30 20:09:34 -0500

    Ski Trip Game

---
+++ Ski Trip/Scripts/Player_Movement.gd
@@ -0,0 +1,15 @@
+extends RigidBody3D
+
+@export var move_speed : float = 2.0
+
+func _physics_process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		linear_velocity.x = -move_speed
+	if Input.is_key_pressed(KEY_RIGHT): 	
+		linear_velocity.x = move_speed
+
+func _on_body_entered(body):
+	if body.is_in_group("Tree"):
+		get_tree().reload_current_scene()
+	if body.is_in_group("Out_Of_Bounds"):
+		get_tree().reload_current_scene()
