07daa86e716e9327ebca1b43ed9475aca22708e4
CodyBenn <zolnyy@yahoo.com>
2023-10-01 23:19:44 -0500

    2D Double Jump

---
+++ First 2D Platformer/Main/Scenes/Player_Controls.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100.0
+@export var gravity : float = 500.0
+@export var jump_force : float = 200.0
+var jump_count : int = 0
+var jump_max : int = 2
+
+func _physics_process(delta):
+	if not is_on_floor() :
+		velocity.y += gravity * delta
+		
+	velocity.x = 0
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += move_speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= move_speed
+		
+	if is_on_floor():
+		jump_count = 0
+	else:
+		if jump_count == 0:
+			jump_count += 1
+		
+	if jump_count < jump_max:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = -jump_force
+			jump_count += 1
+			
+	move_and_slide()
