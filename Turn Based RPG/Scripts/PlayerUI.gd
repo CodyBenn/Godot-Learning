45613f5c48b11e19fbf7641f72a43e3eff7fd594
CodyBenn <zolnyy@yahoo.com>
2023-10-06 23:59:13 -0500

    Trying to find "Visibility" bug

--- Turn Based RPG/Scripts/PlayerUI.gd
+++ Turn Based RPG/Scripts/PlayerUI.gd
@@ -5,21 +5,20 @@ extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("/root/Battlescene").character_begin_turn.connect(_on_character_begin_turn)
-	get_node("/root/Battlescene").character_end_turn.connect(_on_character_begin_turn)
+	get_node("/root/Battlescene").character_end_turn.connect(_on_character_end_turn)
 
 func _on_character_begin_turn(character):
 	visible = character.is_player
 	
 	if character.is_player:
 		_display_combat_actions(character.combat_actions)
-	
-	
+		
 func _on_character_end_turn(character):
 	visible = false
 
 func _display_combat_actions(combat_actions):
 	for i in len(buttons):
-		var button  = get_node(buttons[i])
+		var button = get_node(buttons[i])
 		
 		if i < len(combat_actions):
 			button.visible = true
