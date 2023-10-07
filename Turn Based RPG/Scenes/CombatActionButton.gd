1aa1e6b0f76988540b798a0e89bcc8e1425212b0
CodyBenn <zolnyy@yahoo.com>
2023-10-06 23:14:18 -0500

    Player UI Buttons

--- Turn Based RPG/Scenes/CombatActionButton.gd
+++ Turn Based RPG/Scenes/CombatActionButton.gd
@@ -3,4 +3,4 @@ extends Button
 var combat_action : CombatAction
 
 func _on_pressed():
-	get_node("/root/Battlescene").cur_character.cast_combat_action(combat_action)
+	get_node("/root/Battlescene").cur_char.cast_combat_action(combat_action)
--- Turn Based RPG/Scripts/Turn_Manager.gd
+++ Turn Based RPG/Scripts/Turn_Manager.gd
@@ -14,9 +14,9 @@ signal character_end_turn(character)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().create_timer(0.5).timeout
-	_begin_next_turn()
+	begin_next_turn()
 
-func _begin_next_turn():
+func begin_next_turn():
 	if cur_char == player_char:
 		cur_char = enemy_char
 	elif cur_char == enemy_char:
@@ -32,7 +32,7 @@ func _end_current_turn():
 	await get_tree().create_timer(next_turn_delay).timeout
 	
 	if game_over == false:
-		_begin_next_turn()
+		begin_next_turn()
 		
 func _character_died(character):
 	game_over = true
