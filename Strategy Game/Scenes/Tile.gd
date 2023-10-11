c5c11a4bbf46ba6c0b74bc6356418ff68eb522e9
CodyBenn <zolnyy@yahoo.com>
2023-10-11 12:12:14 -0500

    Tile Script

--- Strategy Game/Scenes/Tile.gd
+++ Strategy Game/Scenes/Tile.gd
@@ -8,3 +8,17 @@ var canPlaceBuilding : bool = false
 #components
 @onready var highlight : Sprite2D = get_node("Highlight")
 @onready var buildingIcon : Sprite2D = get_node("BuildingIcon")
+
+func _ready():
+	add_to_group("Tiles")
+	
+func toggle_highlight(toggle):
+	highlight.visible = toggle
+	canPlaceBuilding = toggle
+	
+func Place_building(buildingTexture):
+	hasBuilding = true
+	buildingIcon.texture = buildingTexture
+
+func _on_input_event(viewport, event, shape_idx):
+	pass
