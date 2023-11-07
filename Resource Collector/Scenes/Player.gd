412c7841f2bde27f7711bb5203da2a0f7af11abe
CodyBenn <zolnyy@yahoo.com>
2023-11-06 23:48:40 -0600

    player movement script mostly complete

--- Resource Collector/Scenes/Player.gd
+++ Resource Collector/Scenes/Player.gd
@@ -1,36 +1,54 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100.0
-var IsInteracting : bool = false
+var isInteracting : bool
+var facingDir = "none"
 
 func _ready():
 	pass
-	
+
 func _physics_process(delta):
-	#Assigns movement based on input
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var interactDirection = direction
-	velocity = direction * move_speed
-	
-	#Animates character with input
-	if direction == Vector2(1,0):
-		$AnimationPlayer.play("walk_right")
-	if direction == Vector2(-1, 0):
-		$AnimationPlayer.play("walk_left")
-	if direction == Vector2(0,1):
-		$AnimationPlayer.play("walk_down")
-	if direction == Vector2(0,-1):
-		$AnimationPlayer.play("walk_up")
-	if Input.is_action_pressed("ui_accept") and Vector2.AXIS_X != 0:
-		IsInteracting = true
-		$AnimationPlayer.play("swing_down")
+
+	# Get the character's input direction
+	var direction = Vector2(0, 0)
+	if isInteracting == false:
+		direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	# Movement input
+	if direction != Vector2(0, 0):
+		velocity = direction * move_speed
+
+		# Set the facing direction for animation
+		if direction.x > 0:
+			facingDir = "right"
+			$AnimationPlayer.play("walk_right")
+		elif direction.x < 0:
+			facingDir = "left"
+			$AnimationPlayer.play("walk_left")
+		elif direction.y > 0:
+			$AnimationPlayer.play("walk_down")
+		elif direction.y < 0:
+			$AnimationPlayer.play("walk_up")
+	else:
+		# If no movement input, set velocity to zero
+		velocity = Vector2(0, 0)
+
+	# Handle interaction
+	if Input.is_action_just_pressed("ui_accept") and !isInteracting:
+		isInteracting = true
 		
-	#Stops animation if no input
-	if velocity == Vector2(0,0) and IsInteracting == false:
+		# Play the attack animation based on facing direction
+		if facingDir == "right":
+			$AnimationPlayer.play("swing_right")
+		if facingDir == "left":
+			$AnimationPlayer.play("swing_left")
+
+	# If not interacting and no movement input, play the idle animation
+	if direction == Vector2(0, 0) and !isInteracting:
 		$AnimationPlayer.play("idle")
-		
+
 	move_and_slide()
-		
+
 func _on_animation_player_animation_finished(animFinish):
-	if animFinish == "swing_down":
-		IsInteracting = false
+	if animFinish == "swing_left" or animFinish == "swing_right":
+		isInteracting = false
---
+++ Resource Collector/Scenes/Player2.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100.0
+var isInteracting : bool
+var facingDir = "none"
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+
+	#Movement if not interacting
+	if isInteracting == false:
+		#Assigns movement physics
+		var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		velocity = direction * move_speed
+		
+		#Plays walk animation based on input
+		if direction == Vector2(1,0):
+			facingDir = "right"
+			$AnimationPlayer.play("walk_right")
+		if direction == Vector2(-1, 0):
+			facingDir = "left"
+			$AnimationPlayer.play("walk_left")
+		if direction == Vector2(0,1):
+			$AnimationPlayer.play("walk_down")
+		if direction == Vector2(0,-1):
+			$AnimationPlayer.play("walk_up")
+			
+		#Plays swing animation
+		if Input.is_action_just_pressed("ui_accept"):
+			isInteracting = true
+			if facingDir == "right":
+				$AnimationPlayer.play("swing_right")
+				move_speed -= 0
+			if facingDir == "left":
+				move_speed -= 0
+				$AnimationPlayer.play("swing_left")
+				
+	#Stops animation if no input
+	if velocity == Vector2(0,0) and isInteracting == false:
+		$AnimationPlayer.play("idle")
+		
+	move_and_slide()
+	
+func _on_animation_player_animation_finished(animFinish):
+	if animFinish == "swing_left" or "swing_right":
+		isInteracting = false
