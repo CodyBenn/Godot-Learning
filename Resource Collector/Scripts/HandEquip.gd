ea992809a7edb49123e3912b4f4695f41e956446
CodyBenn <zolnyy@yahoo.com>
2023-11-10 22:52:46 -0600

    small change

--- Resource Collector/Scripts/HandEquip.gd
+++ Resource Collector/Scripts/HandEquip.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var equippedItem : Resource
 
 func _on_area_2d_body_entered(body):
-#	if(body is ResourceNode):
-#		body.harvest()
-#		if equippedItem == correctEquip:
-#			body.currentResourceAmount -= 1
+	if(body is ResourceNode):
+		body.harvest()
+		if equippedItem == correctEquip:
+			body.currentResourceAmount -= 1
