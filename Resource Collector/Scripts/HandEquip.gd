5ffb2bf8cca0a745b6398c9cf260e8c86e931567
CodyBenn <zolnyy@yahoo.com>
2023-11-12 21:06:05 -0600

    Scripts for equips

--- Resource Collector/Scripts/HandEquip.gd
+++ Resource Collector/Scripts/HandEquip.gd
@@ -1,6 +1,11 @@
-extends Node2D
+extends Sprite2D
 
 @export var equippedItem : Resource
+var correctEquip
+var pickaxe
+var woodAxe
+var waterCan
+var hoe
 
 func _on_area_2d_body_entered(body):
 	if(body is ResourceNode):
