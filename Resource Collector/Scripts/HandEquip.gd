6e1575106326afeffb405b95dbcbaa40e2ba1bf8
CodyBenn <zolnyy@yahoo.com>
2023-11-14 23:47:50 -0600

    HarvestTool Script make

--- Resource Collector/Scripts/HandEquip.gd
+++ Resource Collector/Scripts/HandEquip.gd
@@ -7,12 +7,8 @@ extends Sprite2D
 		self.texture = equippedItem.texture
 		sprite_2d.texture = nextEquipped.texture
 		
+		
 @export var sprite_2d : Sprite2D
-
-var pickaxe
-var woodAxe
-var waterCan
-var hoe
 
 func _on_area_2d_body_entered(body):
 	if(body is ResourceNode):
