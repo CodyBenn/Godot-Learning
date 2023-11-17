7cc28b6fd92cffc8e339f77b334305b00390bc2a
CodyBenn <zolnyy@yahoo.com>
2023-11-17 00:37:05 -0600

    pickup objects and script

--- Resource Collector/Scripts/Pickup.gd
+++ Resource Collector/Scripts/Pickup.gd
@@ -1,11 +1,28 @@
 extends StaticBody2D
+class_name Pickup
 
+@onready var collision_shape : CollisionShape2D = $CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var launch_velocity : Vector2 = Vector2.ZERO
+var move_duration : float = 0
+var time_since_launch : float = 0
+var launching : bool = false :
+	set(is_launching):
+		launching = is_launching
+		
+		collision_shape.disabled = launching
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(launching):
+		position += launch_velocity * delta
+		time_since_launch += delta
+		
+	if(time_since_launch >= move_duration):
+		launching = false
+
+func launch(velocity : Vector2, duration : float):
+	launch_velocity = velocity
+	move_duration = duration 
+	time_since_launch = 0
+	launching = true
+	
--- Resource Collector/Scripts/ResourceNode.gd
+++ Resource Collector/Scripts/ResourceNode.gd
@@ -3,9 +3,12 @@ class_name ResourceNode
 
 @export var resourceType : Array[ResourceNodeType]
 @export var resourceAmount: int = 1
+@export var pickupType : PackedScene
 @export var woodAmount : int
 @export var stoneAmount : int
 @export var dirtAmount : int
+
+@onready var levelParent = get_parent()
 
 var currentResourceAmount : int :
 	set(value):
@@ -16,4 +19,12 @@ func _ready():
 	currentResourceAmount = resourceAmount
 	
 func harvest(amount : int):
+	for n in amount :
+		spawn_resource()
+		
 	currentResourceAmount -= amount
+
+func spawn_resource():
+	var pickup_instance : Pickup = pickupType.instantiate() as Pickup
+	levelParent.add_child(pickup_instance)
+	pickup_instance.position = position
