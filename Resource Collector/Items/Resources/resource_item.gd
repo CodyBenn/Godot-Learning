975da37610d9532a7f85e3e7d9b514154f479122
CodyBenn <zolnyy@yahoo.com>
2023-11-19 22:07:40 -0600

    Player Inventory Script and pickup

---
+++ Resource Collector/Items/Resources/resource_item.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name ResourceItem
+
+@export var display_name : String
+@export var texture : Texture2D
---
+++ Resource Collector/Scripts/Inventory.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name Inventory
+
+@export var resources : Dictionary = { }
+
+func add_resources(type : ResourceItem, amount : int):
+	if(resources.has(type)):
+		resources[type] = resources[type] + amount
+	else:
+		resources[type] = amount
