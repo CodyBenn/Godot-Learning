3f85a2abebeff489ca27345edd2dd9945d1a463f
CodyBenn <zolnyy@yahoo.com>
2023-11-20 19:57:41 -0600

    Inventory script fix

--- Resource Collector/UI/ResourceDisplay.gd
+++ Resource Collector/UI/ResourceDisplay.gd
@@ -12,14 +12,14 @@ func _ready():
 	player_inventory.connect("resource_count_changed", _on_player_inventory_resource_changed)
 	
 func _on_player_inventory_resource_changed(type : ResourceItem, new_count : int) -> void:
-	print("New count for " + type.display_name + " is " +str(new_count))
+#	print("New count for " + type.display_name + " is " +str(new_count))
 	#find existing item display for type
 	var current_display : ResourceItemDisplay
 	
 	#find existing display and update count if one is found
 	for display in displays:
 		if(display.resource_type == type):
-			current_display == display
+			current_display = display
 			current_display.update_count(new_count)
 			break
 			
@@ -29,3 +29,4 @@ func _on_player_inventory_resource_changed(type : ResourceItem, new_count : int)
 		display_container.add_child(new_display)
 		new_display.resource_type = type
 		new_display.update_count(new_count)
+		displays.append(new_display)
