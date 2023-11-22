546a6488a6f2d2816a41d4cd6a2570938feba5c4
CodyBenn <zolnyy@yahoo.com>
2023-11-22 14:27:10 -0600

    inventory setup

--- Resource Collector/UI/ResourceItemDisplay.gd
+++ Resource Collector/UI/ResourceItemDisplay.gd
@@ -4,7 +4,10 @@ class_name ResourceItemDisplay
 @onready var texture_rect : TextureRect = $TextureRect
 @onready var label : Label = $Label
 
-var resource_type : ResourceItem
+var resource_type : ResourceItem :
+	set(new_type):
+		resource_type = new_type
+		texture_rect.texture = resource_type.texture
 
 func update_count(count : int):
-	label.text = str(count)
+	label.text = str("x ", + count)
