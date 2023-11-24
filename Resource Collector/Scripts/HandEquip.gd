d34466d91a4d24603bbe17041e42ee99c1b492ed
CodyBenn <zolnyy@yahoo.com>
2023-11-23 23:53:25 -0600

    Check

--- Resource Collector/Scripts/HandEquip.gd
+++ Resource Collector/Scripts/HandEquip.gd
@@ -1,12 +1,15 @@
 @tool
 extends Sprite2D
 
+
 @export var equipped_item : EquippableItem :
 	set(next_equipped):
 		equipped_item = next_equipped
 		self.texture = equipped_item.texture
 		
+		
 @export var sprite_2d : Sprite2D
+
 
 func _on_area_2d_body_entered(body):
 	if(equipped_item.has_method("interact_with_body")):
