4c78eb0debf8be3dc1a3853dbe37554504ab9d7b
CodyBenn <zolnyy@yahoo.com>
2024-01-27 23:22:52 -0600

    House transition scene and layout
    House transition scene script
    House layout through TileSet

---
+++ Resource Collector/Scripts/EnterBuilding.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export_file("*.tscn") var next_scene
+
+var entered = false
+
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+	
+	
+func _on_body_exited(body: PhysicsBody2D):
+	entered = false
+	
+	
+func _physics_process(_delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file(next_scene)
