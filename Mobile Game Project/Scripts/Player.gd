b24cc3affe08db84607b30582d9b3e84200ff8cc
CodyBenn <zolnyy@yahoo.com>
2024-04-27 15:36:00 -0500

    Movement between screens
    User will move to opposite side of the screen when too far in one direction

--- Mobile Game Project/Scripts/Player.gd
+++ Mobile Game Project/Scripts/Player.gd
@@ -1,28 +1,32 @@
 extends CharacterBody2D
 
+@export var speed = 300.0
+@export var gravity = 15.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var viewport_size
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+func _ready():
+	viewport_size = get_viewport_rect().size
+	
+func _process(delta):
+	pass
+	
 func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	velocity.y = gravity * (speed * .2)
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed * .1)
+	
 	move_and_slide()
+	
+	var margin = 25
+	if global_position.x > viewport_size.x + margin:
+		global_position.x = -margin
+		print("Right to left")
+	if global_position.x < -margin:
+		global_position.x = viewport_size.x + margin
+		print("Left to right")
+		
