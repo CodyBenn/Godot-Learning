2007485deee4c6d4f0ceb66070d6b84074a216fa
CodyBenn <zolnyy@yahoo.com>
2024-05-02 18:56:10 -0500

    Parallax Background
    Setup parallax background with 3 scrolling layers of different speeds, readjusting size with user screen size

--- Mobile Game Project/Scripts/Game.gd
+++ Mobile Game Project/Scripts/Game.gd
@@ -3,6 +3,12 @@ extends Node2D
 @onready var level_generator = $LevelGenerator
 @onready var ground_sprite = $GroundSprite
 
+@onready var parallax1 = $ParallaxBackground/ParallaxLayer
+@onready var parallax2 = $ParallaxBackground/ParallaxLayer2
+@onready var parallax3 = $ParallaxBackground/ParallaxLayer3
+
+var viewport_size: Vector2
+
 #Player variables
 var player_scene = preload("res://Scenes/Player.tscn")
 var player: Player = null
@@ -14,16 +20,35 @@ var camera = null
 
 func _ready():
 	#Grabs screen size for player spawn position
-	var viewport_size = get_viewport_rect().size
+	viewport_size = get_viewport_rect().size
 	#Offsets player so they wont spawn on the bottom of screen
 	var player_spawn_pos_y_margin = 135
 	#Sets player coordinates based on screen size
 	player_spawn_pos.x = viewport_size.x / 2.0
 	player_spawn_pos.y = viewport_size.y - player_spawn_pos_y_margin
 	
+	setup_parallax_layer(parallax1)
+	setup_parallax_layer(parallax2)
+	setup_parallax_layer(parallax3)
+	
 	ground_sprite.global_position.x = viewport_size.x / 2
 	ground_sprite.global_position.y = viewport_size.y
 	new_game()
+	
+func get_parallax_sprite_scale(parallax_sprite: Sprite2D):
+	var parallax_texture = parallax_sprite.get_texture()
+	var parallax_texture_width = parallax_texture.get_width()
+	
+	var parallax_scale = viewport_size.x / parallax_texture_width
+	var result = Vector2(parallax_scale, parallax_scale)
+	return result
+	
+func setup_parallax_layer(parallax_layer: ParallaxLayer):
+	var parallax_sprite = parallax_layer.find_child("Sprite2D")
+	if parallax_sprite != null:
+		parallax_sprite.scale = get_parallax_sprite_scale(parallax_sprite)
+		var my = parallax_sprite.scale.y * parallax_sprite.get_texture().get_height()
+		parallax_layer.motion_mirroring.y = my
 	
 func _process(_delta):
 	#Player controls to quit and reset game using hotkeys
