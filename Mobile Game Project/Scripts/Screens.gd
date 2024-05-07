8bff723297c91122217667f5e0d01c7793bb15fb
CodyBenn <zolnyy@yahoo.com>
2024-05-07 06:08:51 -0500

    Debug buttons
    Connected buttons temporarily for debugging

--- Mobile Game Project/Scripts/Screens.gd
+++ Mobile Game Project/Scripts/Screens.gd
@@ -2,10 +2,17 @@ extends CanvasLayer
 
 @onready var console = $Debug/ConsoleLog
 
+@onready var title_screen = $TitleScreen
+@onready var pause_screen = $PauseScreen
+@onready var game_over_screen = $GameOverScreen
+
+var current_screen = null
+
 func _ready():
 	console.visible = false
 	
 	register_button()
+	change_screen(title_screen)
 	
 func register_button():
 	var buttons = get_tree().get_nodes_in_group("buttons")
@@ -17,8 +24,10 @@ func register_button():
 func _on_button_pressed(button):
 	match button.name:
 		"TitlePlay":
+			change_screen(pause_screen)
 			print("TitlePlay button is pressed")
 		"PauseRestart":
+			change_screen(game_over_screen)
 			print("PauseRestart button is pressed")
 		"PauseMenu":
 			print("PauseMenu button is pressed")
@@ -27,6 +36,7 @@ func _on_button_pressed(button):
 		"GameOverMenu":
 			print("GameOverMenu button is pressed")
 		"GameOverRestart":
+			change_screen(title_screen)
 			print("GameOverRestart button is pressed")
 	#print(button.name)
 	
@@ -42,3 +52,11 @@ func _on_toggle_console_pressed():
 	#else:
 		#console.visible = false
 		#print("Console is turned off")
+		
+func change_screen(new_screen):
+	if current_screen != null:
+		current_screen.disappear()
+	current_screen = new_screen
+	if new_screen != null:
+		current_screen.appear()
+		
---
+++ Mobile Game Project/Scripts/base_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func ready():
+	visible = false
+	
+func appear():
+	visible = true
+	
+func disappear():
+	visible = false
