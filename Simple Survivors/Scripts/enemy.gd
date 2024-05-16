95f1ce9a97b49fbfac63c7caf4046275a55aaca7
CodyBenn <zolnyy@yahoo.com>
2024-05-15 22:23:17 -0500

    Damage script
    Created damage script
    Cleaned up codes
    Connected groups of weapons to enemies to allow for damage
    Created weapon scene for testing purposes

--- Simple Survivors/Scripts/enemy.gd
+++ Simple Survivors/Scripts/enemy.gd
@@ -4,27 +4,53 @@ class_name Enemy
 #Enemy stats
 var level = 1
 @export var movespeed = 150.0
+var current_movespeed: float
 @export var max_health: int = 5
 var current_health:int
-@export var shield: int  = 0
+@export var max_shield: int  = 0
 var current_shield: int
 @export var attack_speed: float = 1.0
 var current_attack_speed: float
 @export var attack_damage: int = 1
 var current_attack_damage
+var damage: int = 1
 
 var player
 
 func _ready():
 	#Gets information to determine player's stats for calculations
 	player = get_node("/root/Main/Player")
+	enemy_stats()
 	
 func _physics_process(_delta):
 	#Combat AI to chase player's position
 	if player:
 		var direction = (player.global_position - global_position).normalized()
-		position += direction * movespeed * _delta  # Multiply by _delta for frame-independent movement
+		position += direction * movespeed * _delta
 		
-#Determines if player is in collision box to assign damage
-func _on_hitbox_body_entered(_body):
-	print("enemy group entered player")
+#Determines what is in collision box to assign damage
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player"):
+		print("enemy group entered player")
+		
+	elif body.is_in_group("weapon"):
+		print("enemy group entered weapon")
+		take_damage(1)
+		print("Enemy took damage. Health pool: " + str(current_health) + " / " + str(max_health))
+		
+#Determines damage taken and deletes if health reaches 0
+func take_damage(amount):
+	current_health -= amount
+	if current_health <= 0:
+		die()
+		
+func die():
+	queue_free()
+	
+#Assigns enemy stats to starting point for spawn
+func enemy_stats():
+	current_health = max_health
+	current_attack_damage = attack_damage
+	current_attack_speed = attack_speed
+	current_shield = max_shield
+	current_movespeed = movespeed
---
+++ Simple Survivors/Scripts/weapon.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("enemy"):
+		print("weapon group entered enemy")
