5ec74d0901e2002a208d6fc563affad1223749bd
CodyBenn <zolnyy@yahoo.com>
2024-05-23 21:03:38 -0500

    Attempt 3
    Almost have damage redone. Enemy not colliding with box, once done it's finished.

--- Simple Survivors/Scripts/weapon.gd
+++ Simple Survivors/Scripts/weapon.gd
@@ -1,13 +1,3 @@
 extends Area2D
 class_name Weapon
 
-var player = Player
-signal player_took_damage
-
-func _process(delta):
-	var overlapping_mobs = %"../Hurtbox".get_overlapping_bodies()
-	if overlapping_mobs.size() > 0:
-		if player.invulnerable:
-			return
-		player.current_health -= overlapping_mobs.size() * delta
-		emit_signal("player_took_damage")
