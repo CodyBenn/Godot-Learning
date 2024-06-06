74ff45133813490661e494ccf857696d46ea664b
CodyBenn <zolnyy@yahoo.com>
2024-06-05 19:11:55 -0500

    Save state
    Sword is being a pain in the a-
    2nd hit in registration isnt working, going to convert from loop to signal and see if it fixes it.

--- Simple Survivors/Scripts/ItemScripts/slash.gd
+++ Simple Survivors/Scripts/ItemScripts/slash.gd
@@ -1,6 +1,14 @@
-extends Area2D
+extends Sword
 
 signal slash_anim_finished
+signal enemy_is_hit
 
-func _on_slash_animation_animation_finished(SlashAnim):
+func _ready():
+	$SlashAnimation.play("SlashAnim")
+
+func _on_slash_animation_finished(SlashAnimation):
 	emit_signal("slash_anim_finished")
+	print("Slash anim finishes")
+
+func _on_area_entered(area):
+	emit_signal("enemy_is_hit", area)
