7848f1451fb5b6714d688f1a5a706464f05dfa79
CodyBenn <zolnyy@yahoo.com>
2024-06-16 17:54:53 -0500

    Buttons and Screens
    Creation of main menu and screen interactions.

--- Mobile Game Project/Scripts/base_screen.gd
+++ Mobile Game Project/Scripts/base_screen.gd
@@ -1,10 +1,23 @@
 extends Control
 
-func ready():
+func _ready():
 	visible = false
+	modulate.a = 0.0
+	
+	get_tree().call_group("buttons", "set_disabled", true)
 
 func appear():
 	visible = true
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 0.5) #Target, Property, Value, Duration
+	return tween
+	
 
 func disappear():
-	visible = false
+	get_tree().call_group("buttons", "set_disabled", true)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.5) #Target, Property, Value, Duration
+	return tween
+	
