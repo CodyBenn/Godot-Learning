3d607512b02de0469fc389fd75d7090b7b6bd483
CodyBenn <zolnyy@yahoo.com>
2024-09-11 20:41:24 -0500

    Update

--- Simple Survivors/Scripts/tutorial.gd
+++ Simple Survivors/Scripts/tutorial.gd
@@ -1,7 +1,6 @@
 extends Label
 
-func _on_timer_timeout():
+func _on_tutorial_end_timer_timeout():
 	var tween = get_tree().create_tween()
 	await tween.tween_property(self, "modulate:a", 0, 1).finished
 	queue_free()
-	
