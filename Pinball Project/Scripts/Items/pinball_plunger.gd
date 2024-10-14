756d946878b115e53ac4a113397db538e4a0a4df
CodyBenn <zolnyy@yahoo.com>
2024-10-13 23:08:57 -0500

    Flippers
    Created new anim for Flippers that doesnt let the ball fall through
    Added new scene for Guards

--- Pinball Project/Scripts/Items/pinball_plunger.gd
+++ Pinball Project/Scripts/Items/pinball_plunger.gd
@@ -9,12 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("left_plunger") and is_in_group("Left_Plunger"):
-		rotation_degrees -= 45
+		$PlungerAnim.play("left_active")
 	if Input.is_action_just_released("left_plunger") and is_in_group("Left_Plunger"):
-		rotation_degrees += 45
+		$PlungerAnim.play("left_default")
 		
 	if Input.is_action_just_pressed("right_plunger") and is_in_group("Right_Plunger"):
-		rotation_degrees += 45
+		$PlungerAnim.play("right_active")
 	if Input.is_action_just_released("right_plunger") and is_in_group("Right_Plunger"):
-		rotation_degrees -= 45
+		$PlungerAnim.play("right_default")
 		
