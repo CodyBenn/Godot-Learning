967c3a4ebb16ebe58999608865fa1c05a8be48e6
CodyBenn <zolnyy@yahoo.com>
2024-10-22 20:43:39 -0500

    Lot of changes
    Pinball monitoring for collisions
    Bumper's collisions and animations
    Small adjustments to corner colliders
    Pinball launcher animations
    Couple of scripts
    Debugging errors corrected

--- Pinball Project/Scripts/Items/pinball_plunger.gd
+++ Pinball Project/Scripts/Items/pinball_plunger.gd
@@ -1,13 +1,7 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("left_plunger") and is_in_group("Left_Plunger"):
 		$PlungerAnim.play("left_active")
 	if Input.is_action_just_released("left_plunger") and is_in_group("Left_Plunger"):
